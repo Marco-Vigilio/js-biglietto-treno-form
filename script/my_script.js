7c4a1d61b11ac74f981bf8b5c37cd4e82cea4df7
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-15 17:00:27 +0200

    gestione dei numeri inseriti negli input

--- script/my_script.js
+++ script/my_script.js
@@ -26,31 +26,47 @@ bottone.addEventListener('click',
             alert("Attenzione non hai inserito correttamente un numero. inserire un numero");
         }
         else{
-            console.log("hai inserito dei numeri bravo");
-            biglietto = biglietto * parseInt(kilomentri.value);
+            if((parseInt(kilomentri.value) > 0) ){
 
-            if(eta.value < 18){
-                console.log("Minorenne sconto del 19.4%");
-                sconto = biglietto * (19.4 / 100);
-                biglietto = biglietto - sconto;
-                biglietto = biglietto.toFixed(2);
-                console.log("Il prezzo del biglietto è di: " + biglietto +"€");
-                document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
-                biglietto = 1;
-            }
-            else if(eta.value >= 65){
-                console.log("Over 65 sconto del 37.7%");
-                sconto = biglietto * (37.7 / 100);
-                biglietto = biglietto - sconto;
-                biglietto = biglietto.toFixed(2);
-                console.log("Il prezzo del biglietto è di: " + biglietto +"€");
-                document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
-                biglietto = 1;
+                if(parseInt(eta.value) >= 0){
+
+                    console.log("hai inserito dei numeri bravo");
+                    biglietto = biglietto * parseInt(kilomentri.value);
+
+                    if(eta.value < 18){
+                        console.log("Minorenne sconto del 19.4%");
+                        sconto = biglietto * (19.4 / 100);
+                        biglietto = biglietto - sconto;
+                        biglietto = biglietto.toFixed(2);
+                        console.log("Il prezzo del biglietto è di: " + biglietto +"€");
+                        document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
+                        biglietto = 1;
+                    }
+                    else if(eta.value >= 65){
+                        console.log("Over 65 sconto del 37.7%");
+                        sconto = biglietto * (37.7 / 100);
+                        biglietto = biglietto - sconto;
+                        biglietto = biglietto.toFixed(2);
+                        console.log("Il prezzo del biglietto è di: " + biglietto +"€");
+                        document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
+                        biglietto = 1;
+                    }
+                    else{
+                        console.log("IL PREZZO DEL BIGLIETTO INTERO E' DI: " + biglietto + "€");
+                        document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
+                        biglietto = 1;
+                    }
+
+                }
+                else if(parseInt(eta.value) > 110){
+                    alert("Complimenti per l'età metti comunque un numero tra 0 - 110");
+                }
+                else{
+                    alert("Attenzione inserire l'età corretta");
+                }
             }
             else{
-                console.log("IL PREZZO DEL BIGLIETTO INTERO E' DI: " + biglietto + "€");
-                document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
-                biglietto = 1;
+                alert("Attenzione, inserire un numero di km più alto di 0");
             }
         }
         console.log();
