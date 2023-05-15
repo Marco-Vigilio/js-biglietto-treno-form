89033531a8bedc706904e6982f10b9dba9608504
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-15 14:41:00 +0200

    file per l'inizializzazione del progetto "Biglietto del treno form"

---
+++ script/my_script.js
@@ -0,0 +1,20 @@
+
+/* 
+Il numero di chilometri da percorrere
+Età del passeggero 
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km (0.233 € al km)
+va applicato uno sconto del 19.4% per i minorenni
+va applicato uno sconto del 37.7% per gli over 65.
+*/
+
+const kilomentri = 0;
+const eta = 0;
+
+document.querySelector("num_km");
+//kilomentri = document.getElementById("num_km.value");
+
+/*
+console.log(document.querySelector("#num_km.value"));
+console.log(document.querySelector("num_age"));
+*/
