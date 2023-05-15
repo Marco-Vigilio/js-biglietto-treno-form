d40510cbb81604554ea28084a7a2c340f4a1f58d
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-15 18:04:16 +0200

    inserimento di <p> con id per gli scontati

--- script/my_script.js
+++ script/my_script.js
@@ -28,27 +28,42 @@ bottone.addEventListener('click',
         else{
             if((parseInt(kilomentri.value) > 0) ){
 
-                if((parseInt(eta.value) >= 0) || (parseInt(eta.value) <= 110)){
+                console.log("Numero di km da percorrere: " + parseInt(kilomentri.value));
+                console.log("Eta dell'utente: " + parseInt(eta.value));
 
-                    console.log("hai inserito dei numeri bravo");
+                document.getElementById("km").innerHTML = "Km: " + parseInt(kilomentri.value);
+                document.getElementById("age").innerHTML = "Eta : " + parseInt(eta.value);
+
+                if((parseInt(eta.value) >= 0) && (parseInt(eta.value) <= 110)){
+
                     biglietto = biglietto * parseInt(kilomentri.value);
 
                     if(eta.value < 18){
                         console.log("Minorenne sconto del 19.4%");
+
+                        document.getElementById("age").innerHTML += "<p id=\"\prezzo_iniziale\"\></p>" + "<p id=\"\discount\"\></p>" ;
+                        document.getElementById("prezzo_iniziale").innerHTML = "Prezzo intero: " + biglietto + "€";
+                        document.getElementById("discount").innerHTML = " Sconto minorenni del 19.4% ";
+
                         sconto = biglietto * (19.4 / 100);
                         biglietto = biglietto - sconto;
                         biglietto = biglietto.toFixed(2);
                         console.log("Il prezzo del biglietto è di: " + biglietto +"€");
-                        document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
+                        document.getElementById("prezzo").innerHTML = "Prezzo finale : " + biglietto + "€";
                         biglietto = 1;
                     }
                     else if(eta.value >= 65){
                         console.log("Over 65 sconto del 37.7%");
+
+                        document.getElementById("age").innerHTML += "<p id=\"\prezzo_iniziale\"\></p>" + "<p id=\"\discount\"\></p>" ;
+                        document.getElementById("prezzo_iniziale").innerHTML = "Prezzo intero: " + biglietto + "€";
+                        document.getElementById("discount").innerHTML = "Sconto over 65 del 37.7% ";
+                        
                         sconto = biglietto * (37.7 / 100);
                         biglietto = biglietto - sconto;
                         biglietto = biglietto.toFixed(2);
                         console.log("Il prezzo del biglietto è di: " + biglietto +"€");
-                        document.getElementById("prezzo").innerHTML = "Prezzo: " + biglietto + "€";
+                        document.getElementById("prezzo").innerHTML = "Prezzo finale : " + biglietto + "€";
                         biglietto = 1;
                     }
                     else{
@@ -59,7 +74,7 @@ bottone.addEventListener('click',
 
                 }
                 else{
-                    alert("Attenzione inserire l'età corretta");
+                    alert("Attenzione inserire l'età compresa tra 0 e 110");
                 }
             }
             else{
